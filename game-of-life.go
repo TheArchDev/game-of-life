51560bd3dc7084f33b8de46877047ddb502c0ef1
Alex Archer <archeralex@gmail.com>
2021-10-04 12:28:30 +0100

    Logic to populate empty 2-dimensional board with changeable dimensions

--- game-of-life.go
+++ game-of-life.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+
 func main() {
 	fmt.Println("Game of Life!")
+	const x_dimension = 2
+	const y_dimension = 2
+	const dead_space = "O"
+	const alive_space = "X"
+	var board [x_dimension][y_dimension]string
+	for x := 0; x < x_dimension; x ++ {
+		for y := 0; y < y_dimension; y ++ {
+	    	board[x][y] = dead_space
+	    }
+	}
+	fmt.Println(board)
 }
