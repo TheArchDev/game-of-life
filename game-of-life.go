778cf493ff5aafe97ac9e0390bd86faf7ee43cf2
David Albert <davidbalbert@gmail.com>
2021-10-05 10:51:12 -0400

    It works!

--- game-of-life.go
+++ game-of-life.go
@@ -10,8 +10,8 @@ import (
 const (
 	Dead_space  = "-"
 	Alive_space = "X"
-	Rows        = 10
-	Columns     = 10
+	Rows        = 40
+	Columns     = 40
 )
 
 func print_board(board [][]string) {
@@ -20,41 +20,61 @@ func print_board(board [][]string) {
 	}
 }
 
-func calculate_state(board [][]string, row int, column int) string {
-	neighbours := [8][2]int{
-		{row - 1, column - 1},
-		{row - 1, column},
-		{row - 1, column + 1},
-		{row, column - 1},
-		{row, column + 1},
-		{row + 1, column - 1},
-		{row + 1, column},
-		{row + 1, column + 1},
+func count_neighbors(board [][]string) [][]int {
+	counts := make([][]int, Rows)
+	for row := 0; row < Rows; row++ {
+		counts[row] = make([]int, Columns)
 	}
-	var valid_neighbours [][2]int
-	for _, neighbour := range neighbours {
-		if -1 < neighbour[0] && neighbour[0] < Rows && -1 < neighbour[1] && neighbour[1] < Columns {
-			valid_neighbours = append(valid_neighbours, neighbour)
+
+	for row := range board {
+		for column := range board[row] {
+			neighbours := [8][2]int{
+				{row - 1, column - 1},
+				{row - 1, column},
+				{row - 1, column + 1},
+				{row, column - 1},
+				{row, column + 1},
+				{row + 1, column - 1},
+				{row + 1, column},
+				{row + 1, column + 1},
+			}
+
+			var valid_neighbours [][2]int
+			for _, neighbour := range neighbours {
+				if -1 < neighbour[0] && neighbour[0] < Rows && -1 < neighbour[1] && neighbour[1] < Columns {
+					valid_neighbours = append(valid_neighbours, neighbour)
+				}
+			}
+
+			alive_neighbour_counter := 0
+			for _, neighbour := range valid_neighbours {
+				neighbour_state := board[neighbour[0]][neighbour[1]]
+				if neighbour_state == Alive_space {
+					alive_neighbour_counter++
+				}
+			}
+
+			counts[row][column] = alive_neighbour_counter
 		}
 	}
 
-	alive_neighbour_counter := 0
-	for _, neighbour := range valid_neighbours {
-		neighbour_state := board[neighbour[0]][neighbour[1]]
-		if neighbour_state == Alive_space {
-			alive_neighbour_counter++
-		}
-	}
+	return counts
+}
 
-	current_cell := board[row][column]
+func next_cell(row, col int, board [][]string, neighbor_counts [][]int) string {
+	current_cell := board[row][col]
 
-	if alive_neighbour_counter == 3 {
+	if neighbor_counts[row][col] == 3 {
 		return Alive_space
-	} else if current_cell == Alive_space && alive_neighbour_counter == 2 {
+	} else if current_cell == Alive_space && neighbor_counts[row][col] == 2 {
 		return Alive_space
 	} else {
 		return Dead_space
 	}
+}
+
+func clear_screen() {
+	fmt.Print("\033[H\033[2J")
 }
 
 func main() {
@@ -77,11 +97,15 @@ func main() {
 	for turn := 0; ; turn++ {
 		fmt.Printf("Turn %v\n", turn)
 		print_board(board)
-		time.Sleep(1 * time.Second)
-		for row := 0; row < Rows; row++ {
-			for column := 0; column < Columns; column++ {
-				new_state := calculate_state(board, row, column)
-				board[row][column] = new_state
+
+		time.Sleep(100 * time.Millisecond)
+		clear_screen()
+
+		counts := count_neighbors(board)
+
+		for row := range board {
+			for column := range board[row] {
+				board[row][column] = next_cell(row, column, board, counts)
 			}
 		}
 	}
