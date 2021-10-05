a70e797b5c69860e0585a7c2d90ce1a6482ed574
Alex Archer <archeralex@gmail.com>
2021-10-05 15:20:02 +0100

    Turn logic running smoothly; still need to address deadvs alive logic properly

--- game-of-life.go
+++ game-of-life.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
 	"time"
-	"math/rand"
 )
 
 const (
-	Dead_space = "-"
+	Dead_space  = "-"
 	Alive_space = "X"
-	Rows = 40
-	Columns = 40
+	Rows        = 40
+	Columns     = 40
 )
 
 func print_board(board [][]string) {
@@ -20,19 +20,49 @@ func print_board(board [][]string) {
 	}
 }
 
-func calculate_state (board [][]string, row int, column int) string {
+func calculate_state(board [][]string, row int, column int) string {
+	fmt.Println(strings.Repeat("=", 50))
+	fmt.Println(row)
+	fmt.Println(column)
 	neighbours := [8][2]int{
-		{ row - 1, column - 1 },
-		{ row - 1, column },
-		{ row - 1, column + 1},
-		{ row, column - 1},
-		{ row, column + 1},
-		{ row + 1, column - 1 },
-		{ row + 1, column },
-		{ row + 1, column + 1},
+		{row - 1, column - 1},
+		{row - 1, column},
+		{row - 1, column + 1},
+		{row, column - 1},
+		{row, column + 1},
+		{row + 1, column - 1},
+		{row + 1, column},
+		{row + 1, column + 1},
 	}
-	fmt.Println(neighbours)
-	return Dead_space
+	fmt.Printf("neighbours: %v\n", neighbours)
+	var valid_neighbours [][2]int
+	for _, neighbour := range neighbours {
+		fmt.Printf("neighbour: %v\n", neighbour)
+		if -1 < neighbour[0] && neighbour[0] < Rows && -1 < neighbour[1] && neighbour[1] < Columns {
+			fmt.Printf("Valid neighbour: %v\n", neighbour)
+			fmt.Printf("neighbour slice: %v\n", neighbour[:])
+			valid_neighbours = append(valid_neighbours, neighbour)
+		}
+	}
+	fmt.Printf("valid_neighbours: %v\n", valid_neighbours)
+
+	alive_neighbour_counter := 0
+	for _, neighbour := range valid_neighbours {
+		fmt.Println(neighbour)
+		neighbour_state := board[neighbour[0]][neighbour[1]]
+		if neighbour_state == Alive_space {
+			alive_neighbour_counter++
+		}
+	}
+	var new_state string
+	if alive_neighbour_counter < 2 {
+		new_state = Dead_space
+	} else if alive_neighbour_counter > 3 {
+		new_state = Dead_space
+	} else {
+		new_state = Alive_space
+	}
+	return new_state
 }
 
 func main() {
