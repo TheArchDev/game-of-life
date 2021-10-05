c4ffbd17c2492c2cc320258eb5547287af84591f
Alex Archer <archeralex@gmail.com>
2021-10-05 15:33:34 +0100

    WIP calculate_state logic

--- game-of-life.go
+++ game-of-life.go
@@ -10,8 +10,8 @@ import (
 const (
 	Dead_space  = "-"
 	Alive_space = "X"
-	Rows        = 40
-	Columns     = 40
+	Rows        = 10
+	Columns     = 10
 )
 
 func print_board(board [][]string) {
@@ -21,9 +21,6 @@ func print_board(board [][]string) {
 }
 
 func calculate_state(board [][]string, row int, column int) string {
-	fmt.Println(strings.Repeat("=", 50))
-	fmt.Println(row)
-	fmt.Println(column)
 	neighbours := [8][2]int{
 		{row - 1, column - 1},
 		{row - 1, column},
@@ -34,35 +31,30 @@ func calculate_state(board [][]string, row int, column int) string {
 		{row + 1, column},
 		{row + 1, column + 1},
 	}
-	fmt.Printf("neighbours: %v\n", neighbours)
 	var valid_neighbours [][2]int
 	for _, neighbour := range neighbours {
-		fmt.Printf("neighbour: %v\n", neighbour)
 		if -1 < neighbour[0] && neighbour[0] < Rows && -1 < neighbour[1] && neighbour[1] < Columns {
-			fmt.Printf("Valid neighbour: %v\n", neighbour)
-			fmt.Printf("neighbour slice: %v\n", neighbour[:])
 			valid_neighbours = append(valid_neighbours, neighbour)
 		}
 	}
-	fmt.Printf("valid_neighbours: %v\n", valid_neighbours)
 
 	alive_neighbour_counter := 0
 	for _, neighbour := range valid_neighbours {
-		fmt.Println(neighbour)
 		neighbour_state := board[neighbour[0]][neighbour[1]]
 		if neighbour_state == Alive_space {
 			alive_neighbour_counter++
 		}
 	}
-	var new_state string
-	if alive_neighbour_counter < 2 {
-		new_state = Dead_space
-	} else if alive_neighbour_counter > 3 {
-		new_state = Dead_space
+
+	current_cell := board[row][column]
+
+	if alive_neighbour_counter == 3 {
+		return Alive_space
+	} else if current_cell == Alive_space && alive_neighbour_counter == 2 {
+		return Alive_space
 	} else {
-		new_state = Alive_space
+		return Dead_space
 	}
-	return new_state
 }
 
 func main() {
